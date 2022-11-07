49e9e0ceea48c7340428f34179ac662e9a45d5d4
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-11-07 18:15:37 +0200

    walk

--- DeltaHospital/Assets/Scripts/Monster.cs
+++ DeltaHospital/Assets/Scripts/Monster.cs
@@ -5,20 +5,50 @@ using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
+    [SerializeField] private Vector3 gotoPosition;
+    private Vector3 initialPosition, destinationPosition, firstMonsterPosition;
     private GameObject monster;
     private bool inSight;//true if the monster can see the player, false otherwise
-    private SightSensor sight;
     [SerializeField] private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         inSight = false;//momentan true pana se face scriptul care l seteaza
+        initialPosition = Vector3.zero;
+        firstMonsterPosition = transform.position;
+
     }
 
-    private bool checkSight()
+    public bool getSight()
     {
-        
+        return inSight;
+    }
+
+
+    private void moveAround()
+    {
+        if (approximatePosition(transform.position, firstMonsterPosition))//check if we arrived at the initial position
+        {
+            GetComponent<NavMeshAgent>().SetDestination(gotoPosition);
+        }
+        else if(approximatePosition(transform.position, gotoPosition))
+        {
+            GetComponent<NavMeshAgent>().SetDestination(firstMonsterPosition);
+        }
+    }
+
+    private bool approximatePosition(Vector3 a, Vector3 b)
+    {
+        Debug.Log("Intru in functie");
+        Vector3Int x = new Vector3Int((int)a.x, (int)a.y, (int)a.z);
+        Vector3Int y = new Vector3Int((int)b.x, (int)b.y, (int)b.z);
+        if (x == y)
+        {
+            Debug.Log("AM AJUNS");
+            return true;
+        }
+        Debug.Log("N am ajuns sau e ceva problema");
         return false;
     }
 
@@ -29,24 +59,33 @@ public class Monster : MonoBehaviour
 
         if (inSight)
         {
-            //follow the player
+            initialPosition = transform.position;//position that the monster has
+            destinationPosition = player.transform.position;
+
             Debug.Log("L am vazut!!!" + extraInformation.transform.tag);
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * extraInformation.distance, Color.green);
-            
-            GetComponent<NavMeshAgent>().SetDestination(player.GetComponent<Transform>().position);
+
+            //follow the player  
+            GetComponent<NavMeshAgent>().SetDestination(player.transform.position);
             transform.LookAt(player.transform);
         }
         else
         {
-            GetComponent<NavMeshAgent>().SetDestination(this.GetComponent<Transform>().position);
+            //GetComponent<NavMeshAgent>().SetDestination(this.GetComponent<Transform>().position);
             Debug.Log("NU VAD NIMIC AJUTOR!");
+            moveAround();
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 20f, Color.red);
         }
-        
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision.gameObject.tag);
+        if(approximatePosition(transform.position, destinationPosition))//the monster arrives in the last seen spot
+        {
+            //send the monster back to its initial position
+            GetComponent<NavMeshAgent>().SetDestination(firstMonsterPosition);
+            if (approximatePosition(transform.position, initialPosition))//check if we arrived at the initial position
+            {
+                //begin the movement
+                initialPosition = Vector3.zero;
+            }
+        }
     }
 }
