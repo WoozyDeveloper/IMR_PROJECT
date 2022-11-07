6ee0903c247ed8a76592c3e58a7c6864f4c348b0
Adrian-Mihai Onofrei <woozydeveloper@gmail.com>
2022-11-07 16:47:48 +0200

    sight of monster

--- DeltaHospital/Assets/Scripts/Monster.cs
+++ DeltaHospital/Assets/Scripts/Monster.cs
@@ -7,24 +7,46 @@ public class Monster : MonoBehaviour
 {
     private GameObject monster;
     private bool inSight;//true if the monster can see the player, false otherwise
+    private SightSensor sight;
     [SerializeField] private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        inSight = true;//momentan true pana se face scriptul care l seteaza
+        inSight = false;//momentan true pana se face scriptul care l seteaza
+    }
+
+    private bool checkSight()
+    {
+        
+        return false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       // if (inSight)
-       // {
+        inSight = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit extraInformation, 20f) && extraInformation.transform.tag == "Player";
+
+        if (inSight)
+        {
             //follow the player
-       //     Debug.Log("Ma duc!!!");
-            this.GetComponent<NavMeshAgent>().SetDestination(player.GetComponent<Transform>().position);
-       //     this.transform.LookAt(player.transform);
-        //}
+            Debug.Log("L am vazut!!!" + extraInformation.transform.tag);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * extraInformation.distance, Color.green);
+            
+            GetComponent<NavMeshAgent>().SetDestination(player.GetComponent<Transform>().position);
+            transform.LookAt(player.transform);
+        }
+        else
+        {
+            GetComponent<NavMeshAgent>().SetDestination(this.GetComponent<Transform>().position);
+            Debug.Log("NU VAD NIMIC AJUTOR!");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 20f, Color.red);
+        }
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
     }
 }
