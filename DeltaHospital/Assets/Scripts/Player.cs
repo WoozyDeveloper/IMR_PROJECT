de541dac88f3f2b9bcdd2e7f72aeafcf67b1f497
Adrian-Mihai Onofrei <59933980+WoozyDeveloper@users.noreply.github.com>
2022-12-30 21:13:36 +0200

    Battery text counter

--- DeltaHospital/Assets/Scripts/Player.cs
+++ DeltaHospital/Assets/Scripts/Player.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int batteryNumber;
+    [SerializeField] private TextMeshProUGUI batteryText;
+
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 250;
         batteryNumber = 0;
+        batteryText.text = batteryNumber.ToString();
     }
 
     public void addBattery()
     {
         batteryNumber++;
+        batteryText.text = batteryNumber.ToString();
     }
 
     // Update is called once per frame
